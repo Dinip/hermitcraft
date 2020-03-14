db81f6a0bae40811391eda09df48e33d28631689
Dinip <dinis_pimpao@hotmail.com>
2020-03-14 01:58:36 +0000

    season1

---
+++ season1/markers.js
@@ -0,0 +1,24 @@
+// Add your own markers to this file.
+
+var MAPCRAFTER_MARKERS = [
+	//Other:
+	{
+		// id of the marker group, without spaces/other special chars
+		"id" : "other",
+		// name of the marker group, displayed in the webinterface
+		"name" : "Other",
+		// icon of the markers belonging to that group (optional)
+		"icon" : "poi.png",
+		// size of that icon
+		"iconSize" : [24, 24],
+		// whether this marker group is shown by default (optional)
+		"showDefault" : true,
+		// markers of this marker group...
+		"markers" : {
+			// ...in the world "world"
+			"world" : [
+				{"pos" : [0, 0, 64], "title" : "Spawn", "icon" : "poi.png", "iconSize" : [24, 24]},
+			],
+		},
+	},
+];
