c696106712617e4aaaad028fd71db24a05a20ecf
TheBulletBot <41111591+TheBulletBot@users.noreply.github.com>
2020-03-24 11:05:25 +0100

    added some more
    added grian's mansion, doc's and Bdubs' base, and Bdubs' hideout of fleeing from doc.

--- season7/markers.js
+++ season7/markers.js
@@ -23,6 +23,10 @@ var MAPCRAFTER_MARKERS = [
 				   {"pos" : [-719, 469, 64], "title" : "Iskall starter base", "icon" : "iskall.png", "iconSize" : [24, 24]},
 				   {"pos" : [-1394, 597, 64], "title" : "Bumbo Baggins' hobbit hole", "icon" : "mumbo.png", "iconSize" : [24, 24]},
 				   {"pos" : [-1192, 862, 64], "title" : "Grian's hobbit hole", "icon" : "grian.png", "iconSize" : [24, 24]},
+				   {"pos" : [1140, -837, 64], "title" : "Doc's half-mansion", "icon" : "doc.png", "iconSize" : [24, 24]},
+				   {"pos" : [1140, -818, 64], "title" : "Bdubs' half-mansion", "icon" : "bdubs.png", "iconSize" : [24, 24]},
+				   {"pos" : [1540, -972, 64], "title" : "Bdubs' hideout (approximate)", "icon" : "bdubs.png", "iconSize" : [24, 24]},
+				   {"pos" : [-1199, 971, 64], "title" : "Grian's main mansion", "icon" : "grian.png", "iconSize" : [24, 24]},
 				
 			],
 		},
