fe603d2ca50adf1b6192664c8bbe6def092ff5cd
cherryredcrimes <67137050+cherryredcrimes@users.noreply.github.com>
2020-06-19 01:43:04 -0700

    Added markers

--- season3/markers.js
+++ season3/markers.js
@@ -24,7 +24,78 @@ var MAPCRAFTER_MARKERS = [
 				{"pos" : [466, 2294, 64], "title" : "Savanna", "icon" : "poi.png", "iconSize" : [24, 24]},
 				{"pos" : [16, 352, 64], "title" : "Quadruple witch hut", "icon" : "poi.png", "iconSize" : [24, 24]},
 				{"pos" : [1264, -1344, 64], "title" : "Horse farm island", "icon" : "poi.png", "iconSize" : [24, 24]},
-				{"pos" : [908, 4292, 64], "title" : "xB's Base", "icon" : "xb.png", "iconSize" : [24, 24]}
+				{"pos" : [5172, 1105, 64], "title" : "Monkeyfarm's Monkey Island", "icon" : "monkeyfarm.png", "iconSize" : [24, 24]}, 
+				{"pos" : [4222, 3300, 64], "title" : "False's airplane and lighthouse", "icon" : "false.png", "iconSize" : [24, 24]},
+				{"pos" : [4098, 3290, 64], "title" : "Creeper Beach", "icon" : "false.png", "iconSize" : [24, 24]},
+				{"pos" : [1172, 700, 64], "title" : "Hermit Hills", "icon" : "poi.png", "iconSize" : [24, 24]},
+				{"pos" : [880, 835, 64], "title" : "Hermit Thrillz", "icon" : "poi.png", "iconSize" : [24, 24]},
+				{"pos" : [8100, 5377, 64], "title" : "Xisuma's Passive Mob Farm", "icon" : "xisuma.png", "iconSize" : [24, 24]},
+				{"pos" : [42, 2962, 64], "title" : "Xisuma's Squid Farm", "icon" : "xisuma.png", "iconSize" : [24, 24]},
+				{"pos" : [245, 2956, 64], "title" : "Impulse's Ghast Catcher", "icon" : "impulse.png", "iconSize" : [24, 24]}, 
+				
+				
+				
+				
+			],
+		},
+	},
+	
+	// Bases
+	{
+		// id of the marker group, without spaces/other special chars
+		"id" : "bases",
+		// name of the marker group, displayed in the webinterface
+		"name" : "Bases",
+		// icon of the markers belonging to that group (optional)
+		"icon" : "poi.png",
+		// size of that icon
+		"iconSize" : [24, 24],
+		// whether this marker group is shown by default (optional)
+		"showDefault" : true,
+		// markers of this marker group...
+		"markers" : {
+			// ...in the world "world"
+			"world" : [
+				{"pos" : [1560, 101, 64], "title" : "Mumbo's Base", "icon" : "mumbo.png", "iconSize" : [24, 24]},
+				{"pos" : [1255, 800, 64], "title" : "Zeuljin's Castle", "icon" : "zueljin.png", "iconSize" : [24, 24]},
+				{"pos" : [1693, 325, 64], "title" : "Cleo's Base", "icon" : "cleo.png", "iconSize" : [24, 24]},
+				{"pos" : [1389, 1755, 64], "title" : "Xisuma's Base", "icon" : "xisuma.png", "iconSize" : [24, 24]},
+				{"pos" : [4136, 3140, 64], "title" : "False's Base", "icon" : "false.png", "iconSize" : [24, 24]},
+				{"pos" : [896, 597, 64], "title" : "Biffa's Base", "icon" : "biffa.png", "iconSize" : [24, 24]},
+				{"pos" : [908, 4292, 64], "title" : "xB's Base", "icon" : "xb.png", "iconSize" : [24, 24]},
+				{"pos" : [-362, 1536, 64], "title" : "Keralis' Base", "icon" : "keralis.png", "iconSize" : [24, 24]},
+				{"pos" : [802, 128, 64], "title" : "Spumwack's Base", "icon" : "spumwack.png", "iconSize" : [24, 24]},
+				{"pos" : [-4023, 3812, 64], "title" : "Sl1pg8r's Base", "icon" : "sl1pg8r.png", "iconSize" : [24, 24]}
+			],
+		},
+	},
+	
+		// Hermit Hills Houses 
+	{
+		// id of the marker group, without spaces/other special chars
+		"id" : "hermithills",
+		// name of the marker group, displayed in the webinterface
+		"name" : "Hermit Hills Houses",
+		// icon of the markers belonging to that group (optional)
+		"icon" : "poi.png",
+		// size of that icon
+		"iconSize" : [24, 24],
+		// whether this marker group is shown by default (optional)
+		"showDefault" : false,
+		// markers of this marker group...
+		"markers" : {
+			// ...in the world "world"
+			"world" : [
+			{"pos" : [1300, 530, 64], "title" : "Hypno's House", "icon" : "hypno.png", "iconSize" : [24, 24]},
+			{"pos" : [1200, 615, 64], "title" : "Joe's House", "icon" : "joe.png", "iconSize" : [24, 24]},
+			{"pos" : [1139, 666, 64], "title" : "Xisuma's House", "icon" : "xisuma.png", "iconSize" : [24, 24]},
+			{"pos" : [1100, 630, 64], "title" : "Jevin's House", "icon" : "xisuma.png", "iconSize" : [24, 24]},
+			{"pos" : [1171, 560, 64], "title" : "False's House", "icon" : "xisuma.png", "iconSize" : [24, 24]},
+			{"pos" : [1150, 750, 64], "title" : "Tango's House", "icon" : "tango.png", "iconSize" : [24, 24]},
+			{"pos" : [1328, 709, 64], "title" : "Impulse's House", "icon" : "xisuma.png", "iconSize" : [24, 24]},
+			{"pos" : [1405, 685, 64], "title" : "Sl1pg8r's House", "icon" : "sl1pg8r.png", "iconSize" : [24, 24]},
+				
+				
 			],
 		},
 	},
