378770940b99e32fe4f0a46553bcf5a4960adfa8
cherryredcrimes <67137050+cherryredcrimes@users.noreply.github.com>
2020-06-19 15:29:23 -0700

    Update markers.js

--- season3/markers.js
+++ season3/markers.js
@@ -31,7 +31,8 @@ var MAPCRAFTER_MARKERS = [
 				{"pos" : [42, 2962, 64], "title" : "Xisuma's Squid Farm", "icon" : "xisuma.png", "iconSize" : [24, 24]},
 				{"pos" : [245, 2956, 64], "title" : "Impulse's Ghast Catcher", "icon" : "impulse.png", "iconSize" : [24, 24]}, 
 				{"pos" : [-1892, 6255, 64], "title" : "False's Christmas Village", "icon" : "false.png", "iconSize" : [24, 24]}, 
-				
+				{"pos" : [3326, 3653, 64], "title" : "Xisuma's Sheep Farm (Rainbowfall)", "icon" : "xisuma.png", "iconSize" : [24, 24]},
+				{"pos" : [6954, 8873, 64], "title" : "Hermit Hallows", "icon" : "poi.png", "iconSize" : [24, 24]}
 				
 				
 				
@@ -56,7 +57,7 @@ var MAPCRAFTER_MARKERS = [
 		"markers" : {
 			// ...in the world "world"
 			"world" : [
-				{"pos" : [1560, 101, 64], "title" : "Mumbo's Base", "icon" : "mumbo.png", "iconSize" : [24, 24]},
+				{"pos" : [1560, 101, 64], "title" : "Mumbo's Mumbowl", "icon" : "mumbo.png", "iconSize" : [24, 24]},
 				{"pos" : [1255, 800, 64], "title" : "Zeuljin's Castle", "icon" : "zueljin.png", "iconSize" : [24, 24]},
 				{"pos" : [1693, 325, 64], "title" : "Cleo's Base", "icon" : "cleo.png", "iconSize" : [24, 24]},
 				{"pos" : [1389, 1755, 64], "title" : "Xisuma's Base", "icon" : "xisuma.png", "iconSize" : [24, 24]},
@@ -65,9 +66,12 @@ var MAPCRAFTER_MARKERS = [
 				{"pos" : [908, 4292, 64], "title" : "xB's Base", "icon" : "xb.png", "iconSize" : [24, 24]},
 				{"pos" : [-362, 1536, 64], "title" : "Keralis' Base", "icon" : "keralis.png", "iconSize" : [24, 24]},
 				{"pos" : [802, 128, 64], "title" : "Spumwack's Base", "icon" : "spumwack.png", "iconSize" : [24, 24]},
-				{"pos" : [-4023, 3812, 64], "title" : "Sl1pg8r's Base", "icon" : "sl1pg8r.png", "iconSize" : [24, 24]},
+				{"pos" : [-4023, 3812, 64], "title" : "Sl1pg8r's Fulcrum", "icon" : "sl1pg8r.png", "iconSize" : [24, 24]},
 				{"pos" : [1316, 368, 64], "title" : "TopMass's Base", "icon" : "topmass.png", "iconSize" : [24, 24]},
 				{"pos" : [-4856, 1832, 64], "title" : "Hypno's Base", "icon" : "hypno.png", "iconSize" : [24, 24]},
+				{"pos" : [1226, 782, 64], "title" : "Impulse's Base (Underground)", "icon" : "impulse.png", "iconSize" : [24, 24]},
+				{"pos" : [1427, 700, 64], "title" : "DMac's Base (In Hill)", "icon" : "dmac.png", "iconSize" : [24, 24]},
+				{"pos" : [-1305, 2017, 64], "title" : "Etho's Base", "icon" : "etho.png", "iconSize" : [24, 24]}
 			],
 		},
 	},
@@ -89,13 +93,19 @@ var MAPCRAFTER_MARKERS = [
 			// ...in the world "world"
 			"world" : [
 			{"pos" : [1300, 530, 64], "title" : "Hypno's House", "icon" : "hypno.png", "iconSize" : [24, 24]},
-			{"pos" : [1200, 615, 64], "title" : "Joe's House", "icon" : "joe.png", "iconSize" : [24, 24]},
+			{"pos" : [1200, 615, 64], "title" : "Joe's Cheers House", "icon" : "joe.png", "iconSize" : [24, 24]},
 			{"pos" : [1139, 666, 64], "title" : "Xisuma's House", "icon" : "xisuma.png", "iconSize" : [24, 24]},
 			{"pos" : [1100, 630, 64], "title" : "Jevin's House", "icon" : "xisuma.png", "iconSize" : [24, 24]},
 			{"pos" : [1171, 560, 64], "title" : "False's House", "icon" : "xisuma.png", "iconSize" : [24, 24]},
 			{"pos" : [1150, 750, 64], "title" : "Tango's House", "icon" : "tango.png", "iconSize" : [24, 24]},
-			{"pos" : [1328, 709, 64], "title" : "Impulse's House", "icon" : "xisuma.png", "iconSize" : [24, 24]},
+			{"pos" : [1328, 709, 64], "title" : "Crown Hall", "icon" : "sl1pg8r.png", "iconSize" : [24, 24]},
 			{"pos" : [1405, 685, 64], "title" : "Sl1pg8r's House", "icon" : "sl1pg8r.png", "iconSize" : [24, 24]},
+			{"pos" : [1343, 571, 64], "title" : "The Hairy Eyeball", "icon" : "tfc.png", "iconSize" : [24, 24]},
+			{"pos" : [1181, 750, 64], "title" : "Mumbo's Hot Air Balloon", "icon" : "mumbo.png", "iconSize" : [24, 24]},
+			{"pos" : [1133, 692, 64], "title" : "xB's House", "icon" : "xb.png", "iconSize" : [24, 24]},
+			{"pos" : [1427, 700, 130], "title" : "Skyzm's House", "icon" : "skyzm.png", "iconSize" : [24, 24]},
+				
+				
 				
 				
 			],
