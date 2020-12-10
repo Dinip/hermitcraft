59524582025333ec857834e36930669089236343
camdendenson1 <75646435+camdendenson1@users.noreply.github.com>
2020-12-10 17:27:25 -0600

    Update markers.js
    This includes many hermitcraft season 1 hermit bases. It also includes the game area and the overworked nether hub.

--- season1/markers.js
+++ season1/markers.js
@@ -19,7 +19,30 @@ var MAPCRAFTER_MARKERS = [
 			"world": [
 				{ "pos": [0, 0, 64], "title": "Spawn", "icon": "poi.png" },
 				{ "pos" : [-815, 587, 64], "title" : "Biffas Base", "icon" : "biffa.png" },
+				{ "pos" : [16355, 7432, 64], "title" : "Hypnotized Base", "icon" : "hypno.png" }, 
+				{ "pos" : [2251, -3974, 64], "title" : "Joehills Base", "icon" : "joe.png" }, 
+				{ "pos" : [398, 507, 64], "title" : "Game District", "icon" : "poi.png" }, 
+				{ "pos" : [560, 506, 64], "title" : "Overworld Nether Hub", "icon" : "poi.png" },
+				{ "pos" : [1263, -664, 64], "title" : "Keralis Base", "icon" : "keralis.png" }, 
+				{ "pos" : [5652, -3319, 64], "title" : "Xisuma's Base", "icon" : "xisuma.png" }, 
+				{ "pos" : [343, 1336, 64], "title" : "Jessassin's Base", "icon" : "jessassin.png" }, 
+				{ "pos" : [2013, -2164, 64], "title" : "Hypnos iron farm", "icon" : "hypno.png" }, 
+				{ "pos" : [265, 2308, 64], "title" : "Topmass Base", "icon" : "topmass.png" }, 
+				{ "pos" : [1258, -659, 64], "title" : "Bdubs caravan", "icon" : "bdubs.png" }, 
+				{ "pos" : [1055, 537, 64], "title" : " Monkeyfarm's Base", "icon" : "monkeyfarm.png" }, 
+
+
+
+
+
+
+
+
+
+
+
 			], 
+
 		},
 	},
 ];
