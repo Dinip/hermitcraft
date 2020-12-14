b415f15728cb3374bd74c57f44b724dbcf3ca91c
thWheelCrafter <75646435+camdendenson1@users.noreply.github.com>
2020-12-14 00:38:07 -0600

    Update markers.js
    This is a fixed version of all the bases and a few farms on the hc  s1 world

--- season1/markers.js
+++ season1/markers.js
@@ -1,5 +1,4 @@
-//Empty format to Copy, Paste { "pos" : [X, Z, 64], "title" : "", "icon" : ".png" }, 
-
+//Empty format to Copy, Paste { "pos" : [X, Z, 64], "title" : "", "icon" : ".png" },
 var MAPCRAFTER_MARKERS = [
 	//Other:
 	{
@@ -19,6 +18,28 @@ var MAPCRAFTER_MARKERS = [
 			"world": [
 				{ "pos": [0, 0, 64], "title": "Spawn", "icon": "poi.png" },
 				{ "pos" : [-815, 587, 64], "title" : "Biffas Base", "icon" : "biffa.png" },
+				{ "pos" : [560, -507, 64], "title" : "Overworld Nether Hub", "icon" : "poi.png" },
+				{ "pos" : [388, -504, 64], "title" : "Game District", "icon" : "poi.png" },
+				{ "pos" : [2013, -2163, 64], "title" : "Hypno's Villager Base and Iron Farm", "icon" : "hypno.png" },
+				{ "pos" : [2249, -3976, 64], "title" : "Joehill's Base", "icon" : "joe.png" },
+				{ "pos" : [2313, -10369, 64], "title" : "CilantroGamer's Base", "icon" : "poi.png" }.
+				{ "pos" : [2914, -592, 64], "title" : "GenerikB's Base", "icon" : "poi.png" },
+		{ "pos" : [1202, -211, 64], "title" : "Shawnvmartin's Base", "icon" : "poi.png" },
+	{ "pos" : [16356, -7432, 64], "title" : "Hypno's Base", "icon" : "hypno.png" },
+	{ "pos" : [20566, -6633, 64], "title" : "Hypno's Slime Farm", "icon" : "hypno.png" },
+	{ "pos" : [3966, 1578, 64], "title" : "Juictra's Base", "icon" : "poi.png" },
+	{ "pos" : [1056, 537, 64], "title" : "KingDaddyDMAC's Base", "icon" : "dmac.png" },
+	{ "pos" : [2811, 3332, 64], "title" : "Red3yz's Base", "icon" : "poi.png" },
+	{ "pos" : [1542, 635, 64], "title" : "Red3yz's Old Base", "icon" : "poi.png" },
+	{ "pos" : [219, 2308, 64], "title" : "Topmass's Base", "icon" : "topmass.png" },
+	{ "pos" : [366, 2369, 64], "title" : "The Pixel Nerd's Base", "icon" : "poi.png" },
+	{ "pos" : [305, 1616, 64], "title" : "IAmSp00n's Base", "icon" : "poi.png" },
+	{ "pos" : [338, 1336, 64], "title" : "Jessassin's Base", "icon" : "jessassin.png" },
+	{ "pos" : [5650, -3326, 64], "title" : "Xisuma's Base(Avalon)", "icon" : "xisuma.png" },
+	{ "pos" : [4772, -2757, 64], "title" : "Xisuma's Slime Farm", "icon" : "xisuma.png" },
+	{ "pos" : [754, -524, 64], "title" : "Aureylian's Base", "icon" : "poi.png" },
+	{ "pos" : [638, -524, 64], "title" : "Cubehamster's Base", "icon" : "poi.png" },
+		
 			], 
 		},
 	},
