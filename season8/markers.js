45c22ccbd16ae4f4ad3a12e54121f1f27d6ae234
Huntracony <Huntracony@users.noreply.github.com>
2021-06-20 18:07:55 +0200

    Added Doc's cursed chunk

--- season8/markers.js
+++ season8/markers.js
@@ -127,6 +127,7 @@ var MAPCRAFTER_MARKERS = [
 				{ "pos": [0, 0, 64], "title": "(0, 0)", "icon": "poi.png" },
 				{ "pos": [860, -690, 64], "title": "Boatem Pole", "icon": "poi.png" },
 				{ "pos": [135, 163, 64], "title": "Spawn", "icon": "poi.png" },
+				{ "pos": [280, -184, 64], "title": "Doc's Cursed Chunk", "icon": "doc.png" },
 				{ "pos": [800, -650, 64], "title": "Tango's Cave of Temptations", "icon": "tango.png" },
 			],
 		},
