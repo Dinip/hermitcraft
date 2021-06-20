66b2763fe48537320f2a5b54912fcc87bef1155f
Huntracony <Huntracony@users.noreply.github.com>
2021-06-20 19:03:36 +0200

    Added Beef's base

--- season8/markers.js
+++ season8/markers.js
@@ -25,6 +25,7 @@ var MAPCRAFTER_MARKERS = [
 				{ "pos": [425, -130, 64], "title": "Doc and Ren's starter base", "icon": "poi.png"},
 				{ "pos": [523, 771, 154], "title": "Etho and Iskall's starter base", "icon": "poi.png"},
 				{ "pos": [1465, 10, 73], "title": "Bdubs' starter base", "icon": "bdubs.png" },
+				{ "pos": [895, -30, 75], "title": "Beef's base", "icon": "beef.png" },
 				{ "pos": [1175, -605, 64], "title": "Cub's starter hole", "icon": "cub.png" },
 				{ "pos": [725, 465, 75], "title": "False's starter base", "icon": "false.png" },
 				{ "pos": [560, 172, 70], "title": "Gemini's starter base", "icon": "gemini.png" },
