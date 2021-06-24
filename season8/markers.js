d537c70c776af680eff4ee2e6c64c4d396a174bd
Dinip <8941012+Dinip@users.noreply.github.com>
2021-06-24 19:32:01 +0100

    Update markers.js

--- season8/markers.js
+++ season8/markers.js
@@ -108,7 +108,7 @@ var MAPCRAFTER_MARKERS = [
 			"world": [
 				{ "pos": [853, -714, 64], "title": "Mumbo's Potato Vending Machine", "icon": "mumbo.png" },
 				{ "pos": [1362, 129, 64], "title": "Mumbo's Potato Vending Machine", "icon": "mumbo.png" },
-				{ "pos": [538,781,154], "title": "Mumbo's Potato Vending Machine", "icon": "mumbo.png" },
+				{ "pos": [538, 781, 154], "title": "Mumbo's Potato Vending Machine", "icon": "mumbo.png" },
 			],
 		},
 	},
