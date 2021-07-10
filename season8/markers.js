d54a9709e5916407817b2591041808051158f9c9
Huntracony <Huntracony@users.noreply.github.com>
2021-07-10 16:50:32 +0200

    Undid adding TEGG because I got it wrong

--- season8/markers.js
+++ season8/markers.js
@@ -66,7 +66,6 @@ var MAPCRAFTER_MARKERS = [
 			// ...in the world "world"
 			"world": [
 				{ "pos": [537, 158, 25], "title": "Gemini's parkour challenge", "icon": "gemini.png" },
-				{ "pos": [860, -715, 64], "title": "TEGG", "icon": "grian.png" },
 				{ "pos": [540, 760, 121], "title": "Iskall's No Wings Club", "icon": "iskall.png" },
 			],
 		},
