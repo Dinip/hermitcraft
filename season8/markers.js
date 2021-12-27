f1547ae98ca4e6df30e9815eb5fdf9c09564adae
MoltenWolfCub <90516933+moltenwolfcub@users.noreply.github.com>
2021-12-27 17:47:49 +0000

    Added Tango's HASA Headquarters
    This is a rough guess based on where Tango flew from Bdubs moon and the nearby island
    
    Tango's vid for him flying to the island: https://youtu.be/L1fzmOhm7pM?t=540

--- season8/markers.js
+++ season8/markers.js
@@ -53,10 +53,8 @@ var MAPCRAFTER_MARKERS = [
 				{ "pos": [850, -860, 64], "title": "Mumbo's Sustainable Utopia", "icon": "mumbo.png" },
 				
 				{ "pos": [850, -622, 64], "title": "Pearl's starter base", "icon": "pearl.png" },
-				{ "pos": [1030, -760, 64], "title": "Pearl's mountains", "icon": "pearl.png" },
 				
 				{ "pos": [900, -630, 64], "title": "Scar's starter base", "icon": "scar.png" },
-				{ "pos": [990, -815, 64], "title": "Scar's snow-capped peaks", "icon": "scar.png" },
 				
 				{ "pos": [835, 101, 55], "title": "Stress' ravine", "icon": "stress.png" },
 				
@@ -230,6 +228,7 @@ var MAPCRAFTER_MARKERS = [
 				{ "pos": [1225, 530, 64], "title": "Tango's horse breeding area", "icon": "tango.png" },
 				{ "pos": [950, 921, 64], "title": "Tango's turtle breeding area", "icon": "tango.png" },
 				{ "pos": [790, -740, 64], "title": "Tango's Bedrock Service", "icon": "tango.png" },
+				{ "pos": [1700, -60, 64], "title": "Tango's HASA Headquaters", "icon": "tango.png" },
 				{ "pos": [976, -267, 65], "title": "TFC's One Night Stand", "icon": "tfc.png" },
 			],
 		},
