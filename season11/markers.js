0cdfb0553ed13801f03bfc6498ffa7220aa7a35f
akr4s1a <132310041+akr4s1a@users.noreply.github.com>
2025-11-09 11:23:05 -0500

    initial season 11 markers, imp-skizz-tango icon (#161)

---
+++ season11/markers.js
@@ -0,0 +1,174 @@
+//Please add markers in Alphabetical order, First .png file, after that marker name, so ZombieCleo is a C because her .png file is cleo.png.
+//Poi markers are on top. Ignore the names in ()
+
+//Empty format to Copy, Paste { "pos" : [X, Z, 64], "title" : "", "icon" : ".png" },
+
+var MAPCRAFTER_MARKERS = [
+
+	// Starter Bases
+	{
+		// id of the marker group, without spaces/other special chars
+		"id": "starters",
+		// name of the marker group, displayed in the webinterface
+		"name": "Starter Bases",
+		// icon of the markers belonging to that group (optional)
+		"icon": "poi.png",
+		// size of that icon
+		"iconSize": [24, 24],
+		// whether this marker group is shown by default (optional)
+		"showDefault": true,
+		// markers of this marker group...
+		"markers": {
+			// ...in the world "world"
+			"world": [
+
+				{ "pos": [1430, 720, 110], "title": "Beef's starter base", "icon": "beef.png" },
+				
+				{ "pos": [2300, 1800, 110], "title": "Cleo's starter base", "icon": "cleo.png" },
+				
+				{ "pos": [1200, 1200, 110], "title": "Cub's starter base", "icon": "cub2.png" },
+				
+				{ "pos": [1100, 2750, 110], "title": "Doc's starter base", "icon": "doc.png"},
+				
+				{ "pos": [1850, 650, 110], "title": "Etho's starter base", "icon": "etho.png"},
+				
+				{ "pos": [1750, 1850, 110], "title": "False's starter base", "icon": "false.png" },
+				
+				{ "pos": [1300, 1550, 110], "title": "Gem's bridge", "icon": "gemini.png" },
+				
+				{ "pos": [1450, 1550, 64], "title": "Grian's starter base", "icon": "grian.png" },
+								
+				{ "pos": [950, 1200, 110], "title": "Jevin's starter base", "icon": "ijevin.png" },
+				
+				{ "pos": [680, 1950, 110], "title": "Impulse, Skizz, Tango starter", "icon": "impulse-skizz-tango.png" },
+				
+				{ "pos": [2220, 1520, 110], "title": "Joe's starter base", "icon": "joe.png" },
+                
+				{ "pos": [1400, 2470, 110], "title": "Joel's starter base", "icon": "joel.png" },
+								
+				{ "pos": [1150, 1450, 110], "title": "Mumbo's starter base", "icon": "mumbo.png" },
+				
+				{ "pos": [2000, 1700, 110], "title": "Pearl's starter base", "icon": "pearl.png" },
+				
+				{ "pos": [2080, 2040, 110], "title": "Ren's starter base", "icon": "rendog.png" },
+				
+				{ "pos": [1550, 1470, 110], "title": "Scar's starter base", "icon": "scar.png" },
+                				
+				{ "pos": [1970, 1440, 110], "title": "Wels' starter base", "icon": "wels.png" },
+				
+				{ "pos": [1150, 1120, 110], "title": "xB's starter base", "icon": "xb.png" },
+				
+				{ "pos": [1200, 1750, 66], "title": "Xisuma's starter base", "icon": "xisuma.png" },
+				
+				{ "pos": [1650, 1250, 64], "title": "Zedaph's starter base", "icon": "zedaph.png" },
+			],
+		},
+	},
+	
+	// Main Bases
+	{
+		// id of the marker group, without spaces/other special chars
+		"id": "bases",
+		// name of the marker group, displayed in the webinterface
+		"name": "Main Bases",
+		// icon of the markers belonging to that group (optional)
+		"icon": "poi.png",
+		// size of that icon
+		"iconSize": [24, 24],
+		// whether this marker group is shown by default (optional)
+		"showDefault": true,
+		// markers of this marker group...
+		"markers": {
+			// ...in the world "world"
+			"world": [
+
+			],
+		},
+	},
+	
+	// Minigames
+	{
+		// id of the marker group, without spaces/other special chars
+		"id": "minigames",
+		// name of the marker group, displayed in the webinterface
+		"name": "Minigames & Event Locations",
+		// icon of the markers belonging to that group (optional)
+		"icon": "poi.png",
+		// size of that icon
+		"iconSize": [24, 24],
+		// whether this marker group is shown by default (optional)
+		"showDefault": true,
+		// markers of this marker group...
+		"markers": {
+			// ...in the world "world"
+			"world": [
+
+			],
+		},
+	},
+	
+	// Farms
+	{
+		// id of the marker group, without spaces/other special chars
+		"id": "farms",
+		// name of the marker group, displayed in the webinterface
+		"name": "Farms",
+		// icon of the markers belonging to that group (optional)
+		"icon": "poi.png",
+		// size of that icon
+		"iconSize": [24, 24],
+		// whether this marker group is shown by default (optional)
+		"showDefault": true,
+		// markers of this marker group...
+		"markers": {
+			// ...in the world "world"
+			"world": [
+								
+			],
+		},
+	},
+
+	
+	// Shops:
+	{
+		// id of the marker group, without spaces/other special chars
+		"id": "shops",
+		// name of the marker group, displayed in the webinterface
+		"name": "Shops",
+		// icon of the markers belonging to that group (optional)
+		"icon": "poi.png",
+		// size of that icon
+		"iconSize": [24, 24],
+		// whether this marker group is shown by default (optional)
+		"showDefault": true,
+		// markers of this marker group...
+		"markers": {
+			// ...in the world "world"
+			"world": [
+								
+			],
+		},
+	},
+	
+	
+	// Other:
+	{
+		// id of the marker group, without spaces/other special chars
+		"id": "other",
+		// name of the marker group, displayed in the webinterface
+		"name": "Other",
+		// icon of the markers belonging to that group (optional)
+		"icon": "poi.png",
+		// size of that icon
+		"iconSize": [24, 24],
+		// whether this marker group is shown by default (optional)
+		"showDefault": true,
+		// markers of this marker group...
+		"markers": {
+			// ...in the world "world"
+			"world": [
+				// { "pos": [0, 0, 64], "title": "Spawn (0, 0)", "icon": "poi.png" },
+			],
+		},
+	},
+];
