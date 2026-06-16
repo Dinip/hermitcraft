7e457b3af2949aaa79f5320469581e60d197e29f
wraycooper211-netizen <wraycooper211@gmail.com>
2026-06-16 14:01:55 +1000

    Update markers.js
    patch 3 fixed impulse.png

--- season11/markers.js
+++ season11/markers.js
@@ -87,7 +87,7 @@ var MAPCRAFTER_MARKERS = [
 			"world": [
 				{ "pos" : [1200, 2100, 64], "title" : "gem's castle's", "icon" : "gemini.png" },
 				{ "pos" : [1350, 1720, 64], "title" : "grian's observatory", "icon" : "grian.png" },
-				{ "pos" : [660, 1970, 64], "title" : "impulse's silo", "icon" : "impluse.png" },
+				{ "pos" : [660, 1970, 64], "title" : "impulse's silo", "icon" : "impulse.png" },
 				{ "pos" : [540, 2000, 64], "title" : "skizz's base", "icon" : "skizz.png" },
 
 			],
@@ -110,7 +110,7 @@ var MAPCRAFTER_MARKERS = [
 		"markers": {
 			// ...in the world "world"
 			"world": [
-				{ "pos" : [550, 24, 64], "title" : "decked out 3", "icon" : ".png" },
+				{ "pos" : [550, 24, 64], "title" : "decked out 3", "icon" : "tango.png" },
 
 			],
 		},
